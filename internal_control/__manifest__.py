02586227e18f995d5329ead5aa9724fceff2b37f
kayode-novajii <kayode.o@novajii.com>
2024-11-21 17:47:09 +0100

    added tree,form view for alert rules

--- internal_control/__manifest__.py
+++ internal_control/__manifest__.py
@@ -18,19 +18,27 @@
     'version': '0.3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'contacts', 'hr', 'access_apps', 'muk_web_theme_default_sidebar_invisible', 'web_field_slider', 'spreadsheet_dashboard', 'hide_powered_by_odoo', 'hide_menu_user','web_widget_numeric_step','legion_hide_odoo','base_automation'],
+    'depends': ['base', 'contacts', 'hr', 'access_apps', 'muk_web_theme_default_sidebar_invisible', 'web_field_slider', 'spreadsheet_dashboard', 'hide_powered_by_odoo', 'hide_menu_user','web_widget_numeric_step','legion_hide_odoo','base_automation', 'icomply_dashboard'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/menus.xml',
+        'views/alert_rules_tree.xml',
+        
     ],
 
     'images': [
     'static/description/icon.png',
     ],
+   
 
     "installable": True,
     "application": True,
     "auto_install": False,
+     'assets':{
+        'web.assets_backend': [
+           
+        ]
+    }
 }
---
+++ internal_control/models/process_category.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class Process_category(models.Model):
+    _name = 'process.category'
+    _description = 'process Category for exception management'
+
+    name = fields.Char(string="Name", required=True)
+    description = fields.Char(string="description", required=True)
+    date_created = fields.Datetime(string="created_at", default=fields.Datetime.now())
+    process_category_id = fields.Many2one('process.category', string="Process Category")
+
+  
