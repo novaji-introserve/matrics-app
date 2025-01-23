e84b538ec27591285da4642bd02a82a7ea178bed
kayode-novajii <kayode.o@novajii.com>
2025-01-23 17:38:48 +0100

    js optimizing

--- internal_control/models/customer.py
+++ internal_control/models/customer.py
@@ -3,21 +3,21 @@ from odoo import models, fields, api
 class Customer(models.Model): 
     _inherit = "res.partner" 
      
-    # customertype = fields.Many2one(
-    #     comodel_name='res.customer.type', string='Customer Type', tracking=True, index=True)
-    # nationality = fields.Many2one(
-    #     comodel_name='res.country', string='Country', tracking=True, index=True)
-    # state_id = fields.Many2one(
-    #     comodel_name='res.country.state', string='State', tracking=True, index=True)
+    customertype = fields.Many2one(
+        comodel_name='res.customer.type', string='Customer Type', tracking=True, index=True)
+    nationality = fields.Many2one(
+        comodel_name='res.country', string='Country', tracking=True, index=True)
+    state_id = fields.Many2one(
+        comodel_name='res.country.state', string='State', tracking=True, index=True)
     occupation = fields.Char(string='Occupation', index=True, tracking=True)
     date_opened = fields.Char(string='Date Opened', index=True, tracking=True)
     address = fields.Char(string='Address', index=True, tracking=True)
     nin = fields.Char(string='NIN', index=True, tracking=True)
-    # status = fields.Many2one(
-    #     comodel_name='res.user.status', string='Status', tracking=True, index=True)
+    status = fields.Many2one(
+        comodel_name='res.user.status', string='Status', tracking=True, index=True)
     phone1 = fields.Char(string='Phone', index=True, tracking=True)
     identification_issue_date = fields.Char(string='identification Issue Date', index=True, tracking=True)
-    # town_id = fields.Many2one(
-    # comodel_name='res.partner.town', string='Town', index=True)
+    town_id = fields.Many2one(
+    comodel_name='res.partner.town', string='Town', index=True)
 
     
