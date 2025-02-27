22be56e09809baec1ec5e6474b4877098bece5fe
Hanson <hanson.e@novajii.com>
2025-02-27 07:44:46 +0100

    fix subbranch

--- internal_control/models/res_subbranch.py
+++ internal_control/models/res_subbranch.py
@@ -4,19 +4,19 @@ class Res_subbranch(models.Model):
     _name = 'res.subbranch'
     _description = 'sub branch'
     _sql_constraints = [
-        ('uniq_subbranchCode', 'unique(subbranchCode)',
+        ('uniq_subbranchCode', 'unique(subbranchcode)',
          "Product already exists. Value must be unique!"),
     ]
 
     
-    subbranchCode = fields.Char()
-    subBranchName = fields.Char()
-    Address = fields.Char()
+    subbranchcode = fields.Char()
+    subbranchname = fields.Char()
+    address = fields.Char()
     email = fields.Char()
-    City = fields.Many2one("res.partner.town")
-    State = fields.Many2one("res.country.state")
-    Country = fields.Many2one("res.country")
-    BranchType = fields.Many2one("res.branchtype")
-    MBranchCode = fields.Many2one("res.branch")
-    Userid = fields.Char(string="User ID")
+    city = fields.Many2one("res.partner.town")
+    state = fields.Many2one("res.country.state")
+    country = fields.Many2one("res.country")
+    branchtype = fields.Many2one("res.branchtype")
+    mbranchcode = fields.Many2one("res.branch")
+    userid = fields.Char(string="User ID")
     authid = fields.Char(string="Auth ID")
