4674483580e99e6e3d6c913976819dc35e328880
kayode-novajii <kayode.o@novajii.com>
2025-03-25 08:43:22 +0100

    customer form now read only

--- compliance_management/__manifest__.py
+++ compliance_management/__manifest__.py
@@ -17,10 +17,24 @@
     "category": "iComply",
     "version": "0.3",
     # any module necessary for this one to work correctly
-    'depends': ['base', 'web', 'bus', 'contacts', 'hr', 'access_apps', 'muk_web_theme_default_sidebar_invisible', 'web_field_slider', 'spreadsheet_dashboard', 'hide_powered_by_odoo', 'hide_menu_user','web_widget_numeric_step','legion_hide_odoo','base_automation'],
-
+    "depends": [
+        "base",
+        "web",
+        "bus",
+        "contacts",
+        "hr",
+        "access_apps",
+        "muk_web_theme_default_sidebar_invisible",
+        "web_field_slider",
+        "spreadsheet_dashboard",
+        "hide_powered_by_odoo",
+        "hide_menu_user",
+        "web_widget_numeric_step",
+        "legion_hide_odoo",
+        "base_automation",
+    ],
     # always loaded
-    'data': [
+    "data": [
         "security/groups.xml",
         "security/ir.model.access.csv",
         "data/res.country.state.csv",
@@ -50,6 +64,7 @@
         "views/customer_accounts.xml",
         "views/transaction.xml",
         "views/transaction_screening_rule.xml",
+        
     ],
     # only loaded in demonstration mode
     "demo": [
@@ -58,37 +73,30 @@
     "installable": True,
     "application": True,
     "auto_install": False,
-    'post_init_hook': 'post_init_hook',
-    'uninstall_hook': 'uninstall_hook',
-    'assets':{
-        'web.assets_backend': [
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
+    "assets": {
+        "web.assets_backend": [
             # Styles
-            'compliance_management/static/src/components/file_upload/scss/csv_import.scss',
-            'compliance_management/static/src/scss/custom_status_bar.scss',
-
+            "compliance_management/static/src/components/file_upload/scss/csv_import.scss",
+            "compliance_management/static/src/scss/custom_status_bar.scss",
             # Load templates first
-            'compliance_management/static/src/components/file_upload/xml/csv_import.xml',
-            
+            "compliance_management/static/src/components/file_upload/xml/csv_import.xml",
             # Then load base files
-            'compliance_management/static/src/components/file_upload/js/terminal.js',
-            'compliance_management/static/src/components/file_upload/js/chunked_uploader.js',
-            'compliance_management/static/src/components/file_upload/js/terminal_component.js',
-            'compliance_management/static/src/components/file_upload/js/import_form_component.js',
-            
+            "compliance_management/static/src/components/file_upload/js/terminal.js",
+            "compliance_management/static/src/components/file_upload/js/chunked_uploader.js",
+            "compliance_management/static/src/components/file_upload/js/terminal_component.js",
+            "compliance_management/static/src/components/file_upload/js/import_form_component.js",
             # Finally load action registrations
             # 'compliance_management/static/src/components/file_upload/js/test_action.js',
-            'compliance_management/static/src/components/file_upload/js/main.js',
-        
-            'compliance_management/static/src/css/style.css',
-            'compliance_management/static/src/components/**/*.js',
-            'compliance_management/static/src/components/**/*.xml',
-            'compliance_management/static/src/components/**/*.css',
-
-
-            'compliance_management/static/src/img/logov.png',
-            'compliance_management/static/img/alt_bank_logo.png',
-            'compliance_management/static/img/alt_bank_logo_.png',
-
+            "compliance_management/static/src/components/file_upload/js/main.js",
+            "compliance_management/static/src/css/style.css",
+            "compliance_management/static/src/components/**/*.js",
+            "compliance_management/static/src/components/**/*.xml",
+            "compliance_management/static/src/components/**/*.css",
+            "compliance_management/static/src/img/logov.png",
+            "compliance_management/static/img/alt_bank_logo.png",
+            "compliance_management/static/img/alt_bank_logo_.png",
         ]
     },
 }
