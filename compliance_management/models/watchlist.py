de5ac2e8ed365c5792c10d93ea4ea2f8c0d932ce
uche-novajii <uche.l@novajii.com>
2025-05-04 23:17:42 +0100

    removed constraints on watchlist table

--- compliance_management/models/watchlist.py
+++ compliance_management/models/watchlist.py
@@ -13,9 +13,10 @@ class Watchlist(models.Model):
     name = fields.Char(string="Name")
     watchlist_id = fields.Char(string="Watchlist ID")
     nationality = fields.Char(string="Nationality")
-    surname = fields.Char(string="Surname",tracking=True,required=True,index=True)
-    first_name = fields.Char(string="First Name",tracking=True,required=True,index=True)
+    surname = fields.Char(string="Surname", tracking=True, index=True)
+    first_name = fields.Char(
+        string="First Name", tracking=True, required=False, index=True)
     middle_name = fields.Char(string="Middle Name")
     customer_id = fields.Many2one(
         comodel_name='res.partner', string='Customer', required=False, index=True)
-    bvn = fields.Char(string='BVN', tracking=True, index = True)
+    bvn = fields.Char(string='BVN', tracking=True, index=True)
