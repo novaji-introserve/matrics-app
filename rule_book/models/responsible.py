23bb63d7ec5c97893ccd787f2bdcc322c93db2dd
uche-novajii <uche.l@novajii.com>
2025-08-26 16:50:37 +0100

    merged

---
+++ rule_book/models/responsible.py
@@ -0,0 +1,20 @@
+# Responsible Model
+from odoo import models, fields, api
+
+class Responsible(models.Model):
+    _name = 'rulebook.responsible'
+    _description = 'Responsible'
+    _rec_name = 'name'
+
+    name = fields.Char(string='Name', required=True, tracking=True)
+    officer_responsible = fields.Char(string='Officer Responsible', required=True, tracking=True)
+    email = fields.Char(string='Email', required=True, tracking=True)
+    cc = fields.Char(string='CC (Email List)', tracking=True)
+    bc = fields.Char(string='BC (Email List)', tracking=True)
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    partner_gid = fields.Char(string="Partner GID")
+    iap_enrich_auto_done = fields.Boolean(string='Automatic Enrichment Done')
